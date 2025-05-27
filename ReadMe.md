e22938cd82e8b66b9720e7c6ad349774c0a34cb6
Halima <natashawanjiku31@gmail.com>
2025-05-27 19:24:26 -0400

    Add the complete project with a read me file

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+#Nationality API
+This project is a nationality project the allows the user to inout their name or full name and inform the user of the country their name is from.
+
+The page is also responsive and can be viewd on all screens
+
+The project is written in
+-HTML
+-CSS
+-JavaScript
--- src/main.js
+++ src/main.js
@@ -14,12 +14,25 @@ btn.addEventListener("click", async (e) => {
     const data = await response.json();
     const { name, country } = data;
 
+    if(!country.length){
+        myResult.textContent = `There are no available countries for that "${name}"`
+        return;
+    }
+    btn.textContent = `Search`;
+
+    if(theName === ""){
+        myResult.textContent = `Enter a valid input`
+        return;
+    }
+
     const theCountries = country.map(
-      (item) => `${item.country_id} (${(item.probability * 100).toFixed(1)}%)`,
+      (item) => ` <li>${item.country_id} (${(item.probability * 100).toFixed(1)}%)</li>`
     );
 
     btn.textContent = `Search`;
-    myResult.textContent = `${name} is from ${theCountries}`;
+
+    
+    myResult.innerHTML = `${name} is from ${theCountries}`;
     console.log(data);
   } catch (error) {
     console.log(error, "something went wrong");
